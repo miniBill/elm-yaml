6b9b73ea32f32fa46d2f60a40283412a729bc13b
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-06-07 12:13:55 +0200

    More test cases

--- tests/TestStringEscape.elm
+++ tests/TestStringEscape.elm
@@ -1,4 +1,4 @@
-module TestStringEscape exposing (fuzzed, suite)
+module TestStringEscape exposing (fuzzed, specificStringParseTest, specificStringsRoundtripTest)
 
 import Ansi.Color
 import Expect
@@ -12,21 +12,46 @@ import Yaml.Parser
 import Yaml.Parser.Ast as Ast
 
 
-suite : Test
-suite =
-    [ ""
-    , "foo"
-    , "\\d"
-    , "\""
-    , "'"
+testCases : List ( String, String )
+testCases =
+    [ ( "\"What is this feature for?\"", "What is this feature for?" )
+    , ( "'{\"food\": \"bacon\"}'", "{\"food\": \"bacon\"}" )
+    , ( "'/bin/bash -c ''echo \"$VALUE\"'''", "/bin/bash -c 'echo \"$VALUE\"'" )
+    , ( "Multiline \\nstring", "Multiline\nstring" )
+    , ( "|\n  literal\n  block\n", "literal\nblock\n" )
+    , ( ">\n  first\n  second\n", "first second\n" )
+    , ( "|-\n  literal\n  block\n", "literal\nblock" )
+    , ( ">-\n  first\n  second\n", "first second" )
+    , ( "\"\"", "" )
+    , ( "\"foo\"", "foo" )
+    , ( "\"\\\\d\"", "\\d" )
+    , ( "\"\\\"\"", "\"" )
+    , ( "\"'\"", "'" )
     ]
+
+
+specificStringParseTest : Test
+specificStringParseTest =
+    testCases
         |> List.map
-            (\value ->
-                Test.test value <|
+            (\( input, output ) ->
+                Test.test ("Parsing check " ++ input) <|
                     \_ ->
-                        checkEncodedStringRoundtrips value
+                        checkIsParsedAs output input
             )
-        |> Test.describe "The string parser"
+        |> Test.describe "Strings get correctly parsed"
+
+
+specificStringsRoundtripTest : Test
+specificStringsRoundtripTest =
+    testCases
+        |> List.map
+            (\( _, output ) ->
+                Test.test ("Roundtrip " ++ escape output) <|
+                    \_ ->
+                        checkEncodedStringRoundtrips output
+            )
+        |> Test.describe "Specific strings roundtrip"
 
 
 fuzzed : Test
@@ -38,20 +63,27 @@ fuzzed =
 
 checkEncodedStringRoundtrips : String -> Expect.Expectation
 checkEncodedStringRoundtrips input =
-    let
-        encoded : String
-        encoded =
-            input
-                |> Json.Encode.string
-                |> Json.Encode.encode 0
-    in
-    case Yaml.Parser.parse encoded of
+    input
+        |> escape
+        |> checkIsParsedAs input
+
+
+escape : String -> String
+escape input =
+    input
+        |> Json.Encode.string
+        |> Json.Encode.encode 0
+
+
+checkIsParsedAs : String -> String -> Expect.Expectation
+checkIsParsedAs expected input =
+    case Yaml.Parser.parse input of
         Err e ->
-            Expect.fail (errorToString encoded e)
+            Expect.fail (errorToString input e)
 
         Ok p ->
             p
-                |> Expect.equal (Ast.String_ input)
+                |> Expect.equal (Ast.String_ expected)
 
 
 errorToString : String -> List DeadEnd -> String
